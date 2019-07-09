c45fba924dacef3e1c554abbad97be7a0dfa1185
David Lamp <david.lamp@workday.com>
2019-07-09 09:29:32 -0600

    Madlibs

--- src/main/java/Course2/Week2/Part2/CodonCount.java
+++ src/main/java/Course2/Week2/Part2/CodonCount.java
@@ -1,5 +1,7 @@
 package Course2.Week2.Part2;
 
+import edu.duke.FileResource;
+
 import java.util.HashMap;
 
 public class CodonCount {
@@ -9,23 +11,54 @@ public class CodonCount {
         codonCount = new HashMap<String, Integer>();
     }
 
-    public static void buildCodonMap(int start, String dna){
+    public static CodonCount buildCodonMap(int start, String dna){
         if (start > 2){
             System.out.println("Nah, bad input!");
-            return;
+            return null;
         }
         CodonCount mappy = new CodonCount();
-        for (int i = start; i < dna.length()-3; i=i+3){
-            String subby = dna.substring(i,i+2);
-            mappy.codonCount.put(subby, mappy.codonCount.get(subby)+1);
+        for (int i = start; i < dna.length()-2; i=i+3){
+            String subby = dna.substring(i,i+3);
+            if (mappy.codonCount.keySet().contains(subby)){
+                mappy.codonCount.put(subby, mappy.codonCount.get(subby)+1);
+            }
+            else {
+                mappy.codonCount.put(subby, 1);
+            }
         }
         for (String key : mappy.codonCount.keySet()){
             System.out.println(key + " was found: " + mappy.codonCount.get(key)+ " times");
         }
+        return mappy;
     }
 
+    public String getMostCommonCodon(){
+        int max = 0;
+        String bestKey = "";
+        for (String key : this.codonCount.keySet()){
+            if (this.codonCount.get(key) > max){
+                max = this.codonCount.get(key);
+                bestKey = key;
+            }
+        }
+        return bestKey;
+    }
+
+    public void printCodonCounts(int start, int end){
+        for (String key : this.codonCount.keySet()){
+            int count = this.codonCount.get(key);
+            if ((count >= start) && (count <= end)){
+                System.out.println("Codon " + key + " was found " + count + " times");
+            }
+            }
+
+        }
+
     public static void testBuildCodonMap(){
-        buildCodonMap(0, "AAABBBCCC");
+        FileResource resource = new FileResource("ReferenceData/Course2Week2MadLibs/ProgrammingImprovingGladLibsData/smalldna.txt");
+        CodonCount mappy = buildCodonMap(2, resource.asString().trim().toUpperCase());
+        System.out.println("Most common codon: " + mappy.getMostCommonCodon());
+        mappy.printCodonCounts(1,5);
     }
 
 }
