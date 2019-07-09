4c962296872fa834b5fe2c4082bff28664be9f89
David Lamp <david.lamp@workday.com>
2019-07-08 22:47:42 -0600

    more commits I guess?

--- src/main/java/Course2/Week2/Part2/CodonCount.java
+++ src/main/java/Course2/Week2/Part2/CodonCount.java
@@ -1,5 +1,31 @@
 package Course2.Week2.Part2;
 
+import java.util.HashMap;
+
 public class CodonCount {
+    private HashMap<String, Integer> codonCount;
+
+    public CodonCount(){
+        codonCount = new HashMap<String, Integer>();
+    }
+
+    public static void buildCodonMap(int start, String dna){
+        if (start > 2){
+            System.out.println("Nah, bad input!");
+            return;
+        }
+        CodonCount mappy = new CodonCount();
+        for (int i = start; i < dna.length()-3; i=i+3){
+            String subby = dna.substring(i,i+2);
+            mappy.codonCount.put(subby, mappy.codonCount.get(subby)+1);
+        }
+        for (String key : mappy.codonCount.keySet()){
+            System.out.println(key + " was found: " + mappy.codonCount.get(key)+ " times");
+        }
+    }
+
+    public static void testBuildCodonMap(){
+        buildCodonMap(0, "AAABBBCCC");
+    }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,14 @@ import Course2.Week2.Part1.GladLib.GladLib;
 public class Main {
 
     public static void main(String[] args){
-        //Course2.Week1.Part1_ImplCaesarCipher.TwoKeysDecrypt.testDoubleDecrypt();
-        //Course2.Week1.Part1_ImplCaesarCipher.CaesarCipher.testCaesarCipher();
-        //Course2.Week1.Part3_ObjCaesarCipher.TestCaesarCipherObj.testCaesarCipherObj();
-        //Course2.Week1.Part3_ObjCaesarCipher.TestCaesarCipherObj2.simpleTest();
-        //Course2.Week2.Part1.WordFrequencies.tester();
-        GladLib libber = new Course2.Week2.Part1.GladLib.GladLib();
-        libber.makeStory();
+//        Course2.Week1.Part1_ImplCaesarCipher.TwoKeysDecrypt.testDoubleDecrypt();
+//        Course2.Week1.Part1_ImplCaesarCipher.CaesarCipher.testCaesarCipher();
+//        Course2.Week1.Part3_ObjCaesarCipher.TestCaesarCipherObj.testCaesarCipherObj();
+//        Course2.Week1.Part3_ObjCaesarCipher.TestCaesarCipherObj2.simpleTest();
+//        Course2.Week2.Part1.WordFrequencies.tester();
+//        GladLib libber = new Course2.Week2.Part1.GladLib.GladLib();
+//        libber.makeStory();
+        Course2.Week2.Part2.CodonCount.testBuildCodonMap();
+
     }
 }
